01f379d2b9bf645135423da062125875a0236f02
sesha-repo <132252094+SeshadriRC@users.noreply.github.com>
2026-04-06 21:25:22 +0530

    Update variables.tf

--- Day-3/modules/S3/variables.tf
+++ Day-3/modules/S3/variables.tf
@@ -1,1 +1,16 @@
-df
+variable "ami_value" {
+    description = "value for the ami"
+}
+
+variable "instance_type_value" {
+    description = "value for instance_type"
+}
+
+variable "aws_region" {
+    description = "AWS Region"
+    type        = string
+}
+
+variable "subnet_id_value" {
+    description = "value for the subnet_id"
+}
