0c66bde5b6bf0457cd44100d69892942c50e1bc9
sesha-repo <132252094+SeshadriRC@users.noreply.github.com>
2026-04-06 21:26:22 +0530

    Update AWS region and AMI in main.tf

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -1,10 +1,14 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 module "ec2_instance" {
   source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" # replace this
+  ami_value = "ami-0a14f53a6fe4dfcd1" # replace this
   instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
+  aws_region = "ap-south-1"
+  subnet_id_value = "subnet-0ccf3b66e816dfd2a"# replace this
+}
+
+module "S3" {
+  source = "./modules/S3"
+  aws_region = "ap-south-1"
+  bucket_name = "my-terraform-bucket-sesha"
+  environment = "dev"
 }
