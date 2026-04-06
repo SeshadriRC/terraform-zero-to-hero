97fcf74172a26f941d5b6b74cbed010f47492402
sesha-repo <132252094+SeshadriRC@users.noreply.github.com>
2026-04-06 21:24:35 +0530

    Update main.tf

--- Day-3/modules/S3/main.tf
+++ Day-3/modules/S3/main.tf
@@ -1,1 +1,12 @@
-s
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
+  }
+}
