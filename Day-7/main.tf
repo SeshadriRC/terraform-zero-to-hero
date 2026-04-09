996bd33df44c2b55869135eb8e5520a82ac970c6
sesha-repo <132252094+SeshadriRC@users.noreply.github.com>
2026-04-09 12:51:49 +0530

    Update main.tf

--- Day-7/main.tf
+++ Day-7/main.tf
@@ -1,32 +1,36 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 provider "vault" {
-  address = "<>:8200"
+  address = "http://3.110.102.28:8200"
   skip_child_token = true
 
   auth_login {
     path = "auth/approle/login"
 
     parameters = {
-      role_id = "<>"
-      secret_id = "<>"
+      role_id = "7431cd02-4a00-eea6-3fc0-73bbec1dbd04"
+      secret_id = "d417922f-7135-4a73-6d12-896957277a1e"
     }
   }
 }
 
+# Now initialize the providers , comment down the below code before running terraform init command
+
 data "vault_kv_secret_v2" "example" {
-  mount = "secret" // change it according to your mount
-  name  = "test-secret" // change it according to your secret
+  mount = "my-key-value" // change it according to your mount
+  name  = "test-secret1" // change it according to your secret
 }
 
+# Now execute terraform apply command to see the output of the secret value, comment down the below code before running terraform apply command.
+
 resource "aws_instance" "my_instance" {
-  ami           = "ami-053b0d53c279acc90"
+  ami           = "ami-0a14f53a6fe4dfcd1"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "test"
-    Secret = data.vault_kv_secret_v2.example.data["foo"]
+    Name = "my-vault-instance"
+    Secret = data.vault_kv_secret_v2.example.data["username"]
   }
 }
